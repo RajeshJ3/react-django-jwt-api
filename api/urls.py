9fc7fd4c95cafbda33c77ac4daab02759759ec57
Rajesh Joshi <joshirajesh448@gmail.com>
2021-04-29 20:36:30 +0530

    added api/ at starting of evert end point

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     
-    path('users/', include('users.urls')),
+    path('api/users/', include('users.urls')),
 ]
